0a30de18505152a37d106c6191a10e69a3446dc8
Volkov <twen1200712@gmail.com>
2021-04-26 20:59:50 +0300

    second

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,24 @@
 "use strict";
 
 // Task 1
+// лучше делать обращение через таргет, что действовало на другие кнопки
 const btn1 = document.querySelector(".hw1");
-
-btn1.addEventListener(
-  "click",
-  ({
-    target: {
-      dataset: { counter },
-    },
-  }) => {
-    if (counter < 1) {
-      btn1.removeEventListener("click", addEventListener());
-    }
-    btn1.setAttribute("data-counter", --counter);
-    btn1.innerHTML = counter;
+const buttonHundler = ({ target }) => {
+  if (target.dataset.counter <= 1) {
+    target.removeEventListener("click", buttonHundler);
   }
-);
+  target.dataset.counter--;
+  target.innerHTML = target.dataset.counter;
+};
+
+btn1.addEventListener("click", buttonHundler);
 // Task 2
 
 const btn2 = document.querySelector(".hw2");
 
 btn2.addEventListener("click", ({ target: { parentNode } }) => {
   function randomNum(a, b) {
-    let randomNumber = Math.floor(Math.random() * (a - b) + b);
+    let randomNumber = Math.floor(Math.random() * (a - b + 1) + b);
     return randomNumber;
   }
   function randomColor(
@@ -31,9 +26,22 @@ btn2.addEventListener("click", ({ target: { parentNode } }) => {
     g = randomNum(0, 255),
     b = randomNum(0, 255)
   ) {
-    return `rgb(${r},${g},${b}`;
+    return `rgb(${r},${g},${b})`;
   }
   parentNode.style.background = randomColor();
 });
 
 // Task 3
+
+const pictureLink = "https://picsum.photos/200/300?random=";
+const img = document.querySelector(".imgs");
+const imgBtn = document.querySelector(".hw3");
+
+imgBtn.addEventListener("click", getImageLink(img));
+
+function getImageLink(elem) {
+  let count = 0;
+  return function () {
+    return (elem.src = pictureLink + count++);
+  };
+}
